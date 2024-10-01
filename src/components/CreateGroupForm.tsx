11d600932c17830a94e84858ebf5abad553b7ee4
MathisZerbib <mathis.zerbib@gmail.com>
2024-10-01 15:14:55 +0200

    linter

--- src/components/CreateGroupForm.tsx
+++ src/components/CreateGroupForm.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
-import { Label } from "../components/ui/label";
 
 interface CreateGroupFormProps {
   onSubmit: (groupName: string, email: string) => Promise<void>;
